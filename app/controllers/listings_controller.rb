cab6ca3e144b982e80209a44d82a8a989ba948d3
James McLaughlin <e.james.mclaughlin@gmail.com>
2016-04-11 19:09:43 -0400

    HW4c

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,37 @@
+class ListingsController < ApplicationController
+  def index
+  	@listings = Listing.all
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+
+    def listing_params
+      params.require(:listing).permit(:title, :description, :price)
+    end
+
+end
