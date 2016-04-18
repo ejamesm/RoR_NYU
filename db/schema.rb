094fa665405a738bcfc56482e02199af33936eba
James McLaughlin <e.james.mclaughlin@gmail.com>
2016-04-18 10:08:44 -0400

    hw5 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406193549) do
+ActiveRecord::Schema.define(version: 20160418134938) do
 
   create_table "features", force: :cascade do |t|
     t.string   "description"
@@ -39,5 +39,15 @@ ActiveRecord::Schema.define(version: 20160406193549) do
   end
 
   add_index "pictures", ["listing_id"], name: "index_pictures_on_listing_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password"
+    t.string   "usertype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
